c0af577cec5bb34605f7ea2fea0577f17451cd53
Deb Das <51954600+ddas94@users.noreply.github.com>
2023-04-04 23:55:11 +0530

    Create LoginPage.jsx
    This code defines a functional component that uses the useState hook to manage the state of the email, password, and error message inputs. The handleSubmit function sends a POST request to the backend API using the axios library, and sets a JWT token in local storage if the login is successful. The component renders a form with email and password inputs, and a submit button. If there is an error, it displays an error message above the form.

---
+++ LoginPage.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/login', { email, password });
+      localStorage.setItem('token', response.data.token);
+      // Redirect to dashboard or another authenticated page
+    } catch (error) {
+      setError('Invalid email or password');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      {error && <div className="error">{error}</div>}
+      <label>Email:</label>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <label>Password:</label>
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
